d49f4b240900c0a8cf45d0787f0a4f29cad94226
iamjanenough <jannilover20@gmail.com>
2025-12-10 00:45:03 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -7,3 +7,22 @@ int main(){
     cout << "#Odd numbers = ";
     return 0;
 }
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,cnt_odd,cnt_even;
+    
+while(true){
+    cout << "Enter an integer: ";
+    cin >> n;
+    if(n == 0) break;
+    
+    if(n%2==0) ++cnt_even;
+    else ++cnt_odd;
+    
+    }
+    cout << "#Even numbers = " << cnt_even << "\n";
+    cout << "#Odd numbers = " << cnt_odd;
+    return 0;
+}
